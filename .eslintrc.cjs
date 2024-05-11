05cdd28bd3795c4fee1aca08381052005b94148d
dawngn <haidangdavid123@gmail.com>
2024-05-11 10:44:22 +0700

    fix: error sapce

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,52 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'react-hooks'],
-  rules: {
-    'no-var': 'error',
-    'no-plusplus': 'off',
-    'no-unused-vars': 'error',
-    'no-param-reassign': 'off',
-    'no-case-declarations': 'off',
-    'quote-props': ['warn', 'as-needed'],
-    'import/prefer-default-export': 'off',
-    // TypeScript rules
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    // React rules
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['ts', 'tsx'],
-      },
-    ],
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
-    'no-console': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'react-hooks'],
+	rules: {
+		'no-var': 'error',
+		'no-plusplus': 'off',
+		'no-unused-vars': 'error',
+		'no-param-reassign': 'off',
+		'no-case-declarations': 'off',
+		'quote-props': ['warn', 'as-needed'],
+		'import/prefer-default-export': 'off',
+		// TypeScript rules
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		// React rules
+		'react/jsx-filename-extension': [
+			'warn',
+			{
+				extensions: ['ts', 'tsx'],
+			},
+		],
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+		'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies
+		'no-console': 'warn',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 }
