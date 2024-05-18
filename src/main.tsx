1adf88fbd7f80510fe30dcf7a503b2e7d40f78f5
dawngn <haidangdavid123@gmail.com>
2024-05-18 21:10:39 +0700

    feat: update src structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Router from '@/routes'
+import Router from '@/routes/index.tsx'
 
 import RootProvider from './features/Providers/index.tsx'
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import react from '@vitejs/plugin-react'
 import million from 'million/compiler'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [million.vite({ auto: true }), react()],
+	base: './',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
 })
